0a18a37dfce700ca3b095a201399502a8af7b3ff
Alexandros Platanios <Alexpl_15@windowslive.com>
2021-09-09 15:27:17 +0300

    v0.0.1

--- bucket_sort.cpp
+++ bucket_sort.cpp
@@ -4,11 +4,8 @@
 #include <iostream>
 using namespace std;
 
-template<
-    typename T,
-    typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type
->
-void min_max(T arr[], int &n, T * min, T * max)
+
+void min_max(int arr[], int &n, int * min, int * max)
 {
     *max = arr[0];
     *min = arr[0];
@@ -21,23 +18,17 @@ void min_max(T arr[], int &n, T * min, T * max)
         }
     }
 }
- 
-
-template<
-    typename T,
-    typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type
->
-void bucketSort(T arr[], int n)
+void bucketSortInt(int arr[], int n)
 {
     // 1). Find Min and Max values
-    T min_val;
-    T max_val;
+    int min_val;
+    int max_val;
     min_max(arr, n, &min_val, &max_val);
     // 2). Get The value increase of every bucket (max_value / array_length)
-    T value_increase = max_val/n;
+    int value_increase = max_val/n;
     // 3). Grab the values (start/stop value of every bucket)
-    T values[n*2];
-    T value = min_val;
+    int values[n*2];
+    int value = min_val;
     for (int i = 0; i < (n*2); i++) {
         if(i > 0) {
             if (i%2 == 0) {
@@ -51,7 +42,7 @@ void bucketSort(T arr[], int n)
         }
     }
     // 4). Create n empty buckets wigth lwngth of the list
-    vector<T> bucket[n];
+    vector<int> bucket[n];
     // 5). Put array elements in the right buckets
     for (int i = 0; i < n; i++) {  // values[i]
 
@@ -75,15 +66,44 @@ void bucketSort(T arr[], int n)
         }
     }
 }
- 
+void bucketSortFloat(float arr[], int n)
+{
+    // 1) Create n empty buckets
+    vector<float> b[n];
+    // 2) Put array elements in different buckets
+    for (int i = 0; i < n; i++) {
+        int bi = n * arr[i]; // Index in bucket
+        b[bi].push_back(arr[i]);
+    }
+    // 3) Sort individual buckets
+    for (int i = 0; i < n; i++) {
+        sort(b[i].begin(), b[i].end());
+    }
+    // 4) Concatenate all buckets into arr[]
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        for (unsigned int j = 0; j < b[i].size(); j++) {
+            arr[index++] = b[i][j];
+        }
+    }
+}
+
 
 
 int main() {
-    //float arr[] = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434, 0.345, 0.222, 0.111, 0.234 };
     int arr[] = { 11, 77, 7, 3, 33, 17, 90, 100, 1, 50, 32, 31, 57, 77, 11 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bucketSort<int>(arr, n);
-    for (int i = 0; i < n; i++)
+    int n1 = sizeof(arr) / sizeof(arr[0]);
+    bucketSortInt(arr, n1);
+    for (int i = 0; i < n1; i++)
         cout << arr[i] << " ";
+
+    cout << endl;
+
+    float arr2[] = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 };
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    bucketSortFloat(arr2, n2);
+    for (int i = 0; i < n2; i++)
+        cout << arr2[i] << " ";
+
     return 0;
 }
